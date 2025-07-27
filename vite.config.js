d60026481d07eb87a48bb09de43172ec25ee5212
yekta_akhavan <yekta.akhavan.sa@gmail.com>
2025-07-27 11:40:31 +0330

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/react-ToDoApp/', // نام ریپازیتوری با دقت
   plugins: [react()],
 })
